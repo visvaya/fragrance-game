5415956348ae59c4876335838e7d13f16bfc2d88
visvaya <maciejbudz29@gmail.com>
2026-01-21 15:28:30 +0100

    perf(analytics): lazy load posthog to reduce TBT

--- components/providers/posthog-provider.tsx
+++ components/providers/posthog-provider.tsx
@@ -6,11 +6,16 @@ import { useEffect } from 'react'
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
     useEffect(() => {
-        posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-            api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-            person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
-            capture_pageview: false, // Disable automatic pageview capture, as we capture manually
-        })
+        // Delay PostHog init to reduce TBT
+        const timer = setTimeout(() => {
+            posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+                api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+                person_profiles: 'identified_only',
+                capture_pageview: false,
+            })
+        }, 2000)
+
+        return () => clearTimeout(timer)
     }, [])
 
     return <PHProvider client={posthog}>{children}</PHProvider>
